fc671b0546409a8f516ad4130e426c3675eae5fd
gradyzhu <gradyzhoo@gmail.com>
2019-02-06 17:15:51 -0800

    Initial commit for rails lite

---
+++ lib/controller_base.rb
@@ -0,0 +1,63 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'active_support/inflector'
+require 'erb'
+require_relative './session'
+require 'byebug'
+
+class ControllerBase
+  attr_reader :req, :res, :params
+
+  # Setup the controller
+  def initialize(req, res)
+    @req = req
+    @res = res
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+    @already_built_response
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+    raise "exception" if already_built_response?
+    @res.status = 302
+    @res['Location'] = url
+    @already_built_response = true
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+    raise "exception" if already_built_response?
+    @res['Content-Type'] = content_type
+    @res.write(content)
+    @already_built_response = true
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+    path = File.dirname(__FILE__)
+    path = path[0..-4]
+    # debugger
+    controller_name = self.class.to_s.underscore
+    file_path = File.join(path, "views", controller_name, "#{template_name}.html.erb")
+    content = File.open(file_path, "r")
+    content_erb = ERB.new(content.read)
+    content.close
+    content_text = content_erb.result(binding)
+    render_content(content_text, 'text/html')
+  end
+
+  # method exposing a `Session` object
+  def session
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+  end
+end
+
